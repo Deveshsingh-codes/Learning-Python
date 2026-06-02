4acd3306c6ed1972c6c68f0c015887754e707529
Devesh Singh <zero2march@gmail.com>
2026-06-02 10:00:26 +0530

    update 303.py

---
+++ 303.py
@@ -0,0 +1,11 @@
+#If else and elif statement in python
+#Write a program to take input of age of a person and if the age is greater than 18 then print "You are eligible to vote" otherwise print "You are not eligible to vote"    
+age=int(input("Enter your age: "))
+if age>18:
+    print("You are eligible to vote")
+elif age==18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")   
+
+    
