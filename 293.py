db1a4c6b1c52b8773babdad2bb62e5bf40cdea73
Devesh Singh <zero2march@gmail.com>
2026-06-02 09:17:59 +0530

    update 293.py

---
+++ 293.py
@@ -0,0 +1,6 @@
+ # Python exercise question 
+ #Create a dictionary and take input form user and return th emeaning of the words from the dictionary.
+d1={"RAM":"Random Access Memory","ROM":"Read Only Memory","CPU":"Central Processing Unit","GPU":"Graphics Processing Unit"}
+Word=input("Enter the word you want to know the meaning of: ")
+print(d1[Word])
+ 
