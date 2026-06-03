84c1cff973681515c65f9be00f58f7dbd86921d7
Devesh Singh <zero2march@gmail.com>
2026-06-03 21:58:16 +0530

    update 311.py

---
+++ 311.py
@@ -0,0 +1,26 @@
+# For loop in python 
+#list1= ["john","richard","fransisca","suort","dran"]
+##print(list1)
+#for item in list1:
+#    print(item)
+list2=["john",2],["richard",7],["fransisca",100]
+for item, pen in list2:
+    print(item, pen)
+dict1=dict(list2)
+for item, pen in list2:
+    print(item, pen)
+    
+    
+    
+    
+    
+    
+    # ---          QUIZ              --- #
+    
+    
+items=[int,float,5,6,5,5,98,'dev']
+for item in items:
+    #if str(item).isnumeric() and item>+6:
+    print(item)
+        
+        
