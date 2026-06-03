a3d48ebbb5a8c5289d5aeabc03a3f9014c4257e3
Devesh Singh <zero2march@gmail.com>
2026-06-03 22:18:26 +0530

    update 313.py

---
+++ 313.py
@@ -0,0 +1,8 @@
+# Break statement in Python
+i=0
+while(True):
+    print(i+1,end=" ")
+    if(i==5):
+        break#break statement is used to exit the loop when a certain condition is met. In this case, the loop will continue until i equals 5, at which point it will break out of the loop.
+    i=i+1
+    
