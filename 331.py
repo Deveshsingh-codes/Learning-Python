dbc947c695f9179735e79c46b754d2373aef18e1
Devesh Singh <zero2march@gmail.com>
2026-06-05 00:54:55 +0530

    update 331.py

--- 331.py
+++ 331.py
@@ -1,5 +1,24 @@
  # Python task 
  # 1.---- GUESS THE NUMBER ----
  
+from numbers import Number
+
+
 n = 134
- 
+print("Number of guesses is limited to only 9 times")
+Number_of_guesses = 0
+while(Number_of_guesses<=9):
+    guess = int(input("Guess the number: "))
+    if guess<n:
+        print("You entered a smaller number, try again!")
+    elif guess>n:
+        print("You entered a greater number, try again!")
+    else:
+        print("Congratulations! You guessed the number in ", Number_of_guesses, "guesses")
+        break
+    Number_of_guesses+=1
+if(Number_of_guesses>9):
+    print("Game over")
+        
+        
+    
