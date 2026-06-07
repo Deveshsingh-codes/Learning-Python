987aca02a4cbf1e15602d8da4fac35dc63c6d15c
Devesh Singh <zero2march@gmail.com>
2026-06-07 22:52:52 +0530

    update 344.py

---
+++ 344.py
@@ -0,0 +1,7 @@
+# Seek(), tell() and readline() methods
+f=open("Devesh.txt","r") # r se file read mode me khulti hai
+print(f.read()) # read() method se hum file ka content read kar sakte hai
+print(f.tell()) # tell() method se hum file pointer ki current position dekh sakte  
+print(f.seek(0)) # seek() method se hum file pointer ki position change kar sakte hai, yaha 0 se file pointer ko start me le jate hai
+print(f.readline()) # readline() method se hum file ka ek line read kar sakte hai
+f.close() # file ko close karna jaruri hai, taki memory free ho jaye
