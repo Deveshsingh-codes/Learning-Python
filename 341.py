53df0a5154ec5fbe2f329c16a10a9bacf8fe1f7b
Devesh Singh <zero2march@gmail.com>
2026-06-07 22:41:39 +0530

    update 341

---
+++ 341.py
@@ -0,0 +1,11 @@
+#File IO Basic
+"r"  # open file for reading ----Default----
+"w" # open file for writing
+"a" # open file for appending at the end of the file
+"x" # open file for exclusive creation, if the file already exists, the operation will fail                                     
+"t" # open file in text mode ----Default----
+"b" # open file in binary mode
+"+" # open file for updating (reading and writing)  
+
+
+# Question of the day----
