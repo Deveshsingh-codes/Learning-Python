a7347a239d7e3638ce62736d43de4132c88c06ff
Devesh Singh <zero2march@gmail.com>
2026-06-08 22:31:00 +0530

    Initial commit

---
+++ 332.py
@@ -0,0 +1,6 @@
+#python excercise : Astrologer's star
+n = int(input("Enter the number of rows: "))
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end="")
+    print()
--- 342.py
+++ 342.py
@@ -2,4 +2,4 @@
 f=open("Devesh.txt")
 content=f.read()
 print(content)
-f.close()
+f.close() # Ek baar agr koi file open kri hai to usko close bhi krna pdt hi hai....
