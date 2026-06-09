e3bf7b5460e4b84b97c280c0eb1f270c49519805
Devesh Singh <zero2march@gmail.com>
2026-06-09 12:36:57 +0530

    Update 342.py

--- 342.py
+++ 342.py
@@ -1,5 +1,20 @@
 #File Writing
-f=open("Devesh.txt")
-content=f.read()
+f=open("Devesh.txt","rb") #it gives output in binary formate
+f=open("Devesh.txt","rt")
+content=f.read(3)
 print(content)
 f.close() # Ek baar agr koi file open kri hai to usko close bhi krna pdt hi hai....
+
+f=open("Devesh.txt","rt")
+content=f.read()
+for line in content:
+    print(line) #this print one text in one line 
+for line in f:
+    print(line,end="")
+print(f.readline())
+print(f.readline())
+
+print(f.readlines())
+f.close()
+
+    
