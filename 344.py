3100bf1774770ada546c4ba8e0eb73bd10dd9932
Devesh Singh <zero2march@gmail.com>
2026-06-13 21:13:27 +0530

    update 344.py

--- 344.py
+++ 344.py
@@ -5,3 +5,9 @@ print(f.tell()) # tell() method se hum file pointer ki current position dekh sak
 print(f.seek(0)) # seek() method se hum file pointer ki position change kar sakte hai, yaha 0 se file pointer ko start me le jate hai
 print(f.readline()) # readline() method se hum file ka ek line read kar sakte hai
 f.close() # file ko close karna jaruri hai, taki memory free ho jaye
+
+
+print(f.readline())
+f.seek(0)
+f.close()
+ 
