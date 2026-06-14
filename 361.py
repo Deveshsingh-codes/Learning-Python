c77e7c07bcf0d8ea1063fb602bd9ba6e4f5ec5d1
Devesh Singh <zero2march@gmail.com>
2026-06-14 12:52:31 +0530

    update 361.py

--- 361.py
+++ 361.py
@@ -1,10 +1,10 @@
-# Recursions :Recursive vs iterative approach
-#def print2(str1):
-#    print(str1)
-#    print("this is  " + str1)
-#print2("devesh")    
+## Recursions :Recursive vs iterative approach
+def print2(str1):
+    print(str1)
+    print("this is  " + str1)
+print2("devesh") 
 
-#<--------------------->
+#<------------iterative method--------->
 def factorial(n):
 # n! =n*(n-1)*(n-2)*...1
 # n! = n*(n-1)!
@@ -18,4 +18,19 @@ def factorial(n):
     """
 number=int(input("Enter the number ="))
 print(factorial(number))
-    
+
+#<-------Recursive method----->
+def factorial_recursive(n):
+    if n==1:
+        return 1
+    else:
+        return n*factorial_recursive(n-1)
+number = int(input("Enter the number ="))
+print(factorial_recursive(number))
+
+
+###WORKING###
+#5*factorial_recursive(n-1)
+#5*4*factorial_recursive(n-1)
+#5*4*3*factorial_recursive(n-1)
+#5*4*3*2*factorial_recursive(1)....#factorial_recursive(1)==1
