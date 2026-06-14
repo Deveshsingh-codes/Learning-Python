7b9298cf8902b79a8cdc6e357f67abd89587ea1a
Devesh Singh <zero2march@gmail.com>
2026-06-14 17:59:36 +0530

    update 365.py

---
+++ 365.py
@@ -0,0 +1,12 @@
+#USING PYTHON EXTERNAL AND BUILT IN MODULES#
+import random
+
+random_number = random.randint(0, 5)
+print("Random Number:", random_number)
+ 
+rand=random.random()*100
+print(rand) 
+
+lst=["o2","co2","c","n"]
+choice=random.choice(lst)
+print(choice)
