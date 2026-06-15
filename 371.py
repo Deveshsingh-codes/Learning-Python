2d222006fff4d7cbcb63797e95f84179e7ac6f0b
Devesh Singh <zero2march@gmail.com>
2026-06-15 18:08:03 +0530

    Updaet 371.py

--- 371.py
+++ 371.py
@@ -1,6 +1,12 @@
-## F-STRING AND STRING FORMATTING IN PYTHON
-me=" Devesh Singh"
-a1= 55  
-a="This is {1}{0}"
-b=a.format(me,a1)
-print(b)
+### F-STRING AND STRING FORMATTING IN PYTHON
+#me=" Devesh Singh"
+#a1= 55  
+#a="This is {1}{0}"
+#b=a.format(me,a1)
+#print(b)
+
+
+you=" Aditya singh"
+a2=3
+c=f"This is {you} {a2} {4*654}"
+print(c)
