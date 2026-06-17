25549d05ee6ab260056032355e4666e870f3d2f2
Devesh Singh <zero2march@gmail.com>
2026-06-17 22:06:51 +0530

    Update 376.py

--- 376.py
+++ 376.py
@@ -3,7 +3,13 @@ l1=["Bhindi","Aloo","Tea","Soap","Bag"]
 
 i=1
 for item in l1:
-    if i%2!=0:
+    if i%2!=0: 
         print(f"{item}")
     i+=1
+    
+    
+    
+for index, item in enumerate(l1):
+    if index%2==0:
+        print(f"Jarvis please buy{item}")
     
