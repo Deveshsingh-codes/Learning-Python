5cabee4de81191c40e5d6bb99dcc163b81601780
Devesh Singh <zero2march@gmail.com>
2026-06-17 21:56:04 +0530

    Update 374.py

--- 374.py
+++ 374.py
@@ -7,4 +7,11 @@ k=0
 while(k<45):
     print("hey")
     k+=1
+print(time.time()-initial) #The time in which while loop execute.
+
+initial2=time.time()
+for i in range(1):
+    print("This is harry bhai.")
+print("For loop ran in ",time.time()-initial2)
+
     
