1744bf0c3fa050279fc1cabcf9d8b78a7ee4b94d
Devesh Singh <zero2march@gmail.com>
2026-06-17 22:14:47 +0530

    Update 381.py

--- 381.py
+++ 381.py
@@ -3,3 +3,13 @@
 #print(sk.__version__)
 import sys
 print(sys.path)
+
+import file2
+file2.a
+
+from file2 import a
+print(a) #is se compilor confuse ho ajyega aur kisi bhi file ka a print kr dega...
+
+import file2
+print(file2.a)
+file2.printjoke("This is me")
