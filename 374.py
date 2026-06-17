2bd988d2d16fd8d95d37e33ba8c453e8ad569b1b
Devesh Singh <zero2march@gmail.com>
2026-06-17 22:01:22 +0530

    Update 374.py

--- 374.py
+++ 374.py
@@ -2,7 +2,10 @@
 import time
 
 initial=time.time()
-print(initial)
+print(initial) #It th enumber of ticks
+
+#print(time.sleep(2)) #it gives the output with the break of 2 seconds 
+
 k=0
 while(k<45):
     print("hey")
@@ -12,6 +15,10 @@ print(time.time()-initial) #The time in which while loop execute.
 initial2=time.time()
 for i in range(1):
     print("This is harry bhai.")
-print("For loop ran in ",time.time()-initial2)
+print("For loop ran in ",time.time()-initial2) #The time in which For loop execute.
+
+
+localtime=time.asctime(time.localtime(time.time()))
+print(localtime)
 
     
