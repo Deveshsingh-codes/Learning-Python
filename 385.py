925c2e625b3d8dd83d76558e3678d64eba017ae5
Devesh Singh <zero2march@gmail.com>
2026-06-19 21:47:25 +0530

    Update 385.py

--- 385.py
+++ 385.py
@@ -2,4 +2,15 @@
 def fun1():
     print("Subscribe now")
     
-fun2=fun1
+fun2=fun1 ##()---agr ye paranthisis lga diya to function call ho jayega...
+#abhi ye same output dega because fun2 me phl ehi copy ho chuka hai...
+fun2()
+
+
+
+def funcret(num):
+    if num ==0:
+        return print
+    if num ==1:
+        return int
+        #return sum  ##Function ka use krke function bhi call kr skte hai..
