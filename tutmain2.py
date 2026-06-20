3656475f4b62d76389244e4f95f821f51fd634cb
Devesh Singh <zero2march@gmail.com>
2026-06-20 15:31:28 +0530

    Update tutmain2.py

--- tutmain2.py
+++ tutmain2.py
@@ -1,2 +1,12 @@
 import tutmain1
-print(tutmain1.add(5,3))
+print(tutmain1.add(5,3)) 
+
+#function ko import krte krte fun ko execute bhi kr deta hai...
+#...jb hm isko add kr rhe hai tb ye 15 de rha hai is executio ko rokne ke liye ...
+# we use if __name__ == '__main__':
+# ab ye 13 dega 
+
+
+print("The name is ", __name__)
+#tutmain1---->main
+#tutmain2---->tutmain1
