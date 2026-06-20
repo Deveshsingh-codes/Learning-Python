2b87353b537b8b2820ec190d588cdd52fa07a952
Devesh Singh <zero2march@gmail.com>
2026-06-20 15:55:51 +0530

    Update 383.py

--- 383.py
+++ 383.py
@@ -1,6 +1,6 @@
 #Join Function
 lis=["john","cena","randy","ortan","khali"]
 for item in lis:
-    print(item,"and",end="")
+    print(item,"and",end=" ")
 d="and".join(lis)
 print("other wwe superstar")
