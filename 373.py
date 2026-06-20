be02a943f681610f894b3ff294c98e897294acaf
Devesh Singh <zero2march@gmail.com>
2026-06-20 13:55:09 +0530

    Update 373.py

--- 373.py
+++ 373.py
@@ -1,6 +1,31 @@
 ###----------*arg and **kwarg in python--------->
+def fun_name_print(a, b, c, d):
+    print(a, b, c, d)
+fun_name_print("Harry", "Devesh", "Surya", "Chhotu") #Function call krdiya
+#Its not a scalable function... agr ek naam aur badha diya to ye erro dedega...ese agr ek company ke employees ka krna pdega to bhut bada code bna jayega aur ek achha method nhi hoga 
+# the n we use *args
+
+
+    
 def funargs(*args):
-    if args:
-        print(args[0])
-    else:
-        print('No arguments provided')
+        print(*args) #ye puri list print kr dega
+        #print(args[0]) ...it prints first element of list 
+har=["Harry", "Devesh", "Surya", "Chhotu"] #ye ek conversion bhi jo jaat hai list ka tuple me 
+funargs(*har)
+# Now its a good method to add employees name in list
+
+
+#Normal argument--->
+def funargs(normal,*args):
+    print(normal,*args)
+har=["Harry", "Devesh", "Surya", "Chhotu"]  
+normal= "I am normal argument and the students are"
+funargs(normal,*har)
+
+
+
+def funargs(*args):
+    for items in args:
+        print (items)
+har=["Harry", "Devesh", "Surya", "Chhotu"]
+funargs(*har)
