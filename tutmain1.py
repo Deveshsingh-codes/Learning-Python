3ea44f9212a12fa74feee86e481fa162ecdc5ee7
Devesh Singh <zero2march@gmail.com>
2026-06-20 15:29:08 +0530

    Update tutmain1.py

--- tutmain1.py
+++ tutmain1.py
@@ -1,4 +1,18 @@
 def printhar(string):
-    return "Ye string harry ko dede taki use{string}"
+    return f"Ye string harry ko dede taki use {string}"
 def add (num1,num2):
     return num1+num2+5 
+print(printhar("Harry1"))
+O =add(4,6)
+print(O)
+
+
+print("The name is ", __name__)
+#tutmain1---->main
+#tutmain2---->tutmain1
+
+
+if __name__ == '__main__':
+   print(printhar("Harry1"))
+   O = add(4,6)
+   print(O)
