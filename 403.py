34cb4c21c8a509978737b76d7137ade879ca0806
Devesh Singh <zero2march@gmail.com>
2026-06-22 22:17:40 +0530

    Update 403.py

--- 403.py
+++ 403.py
@@ -2,3 +2,11 @@
 
 class Employee: #It is an empty class
     pass  # Pass = Nothing
+
+class Employee:
+    no_of_leaves=8 # these are the leaves of this class
+Harry=Employee()
+Rohan=Employee()
+
+Harry.name="Harry"
+Rohan.name="Rohan"
