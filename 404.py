6a40589a23e8b35b1c90730feffdb33723903094
Devesh Singh <zero2march@gmail.com>
2026-06-22 23:03:14 +0530

    Updaet 404.py

--- 404.py
+++ 404.py
@@ -1,7 +1,20 @@
-# Next to the instance and class varable...
+#Self and __init__()  [Constructor]
 
-class Employee:
-    def developers(self):
-        harry="harry"
-        Rohan="Rohan"
-    print (developers)
+class Employee():
+    no_of_leaves=8
+    def printdetails(self): 
+        # self means ek esa object jiski hm baat kr rhe hai
+        # ek instance jis pe function lgaya ja rha hai ....
+        return f"Name is {self.name} salary is {self.salary} and role is  {self.role}"
+Harry=Employee()
+Rohan=Employee()
+
+Harry.name="Harry"
+Rohan.name="Rohan"
+Harry.salary=400
+Rohan.salary=500
+Harry.role="Instructor"
+Rohan.role="Student"
+
+print(Rohan.printdetails())    
+        
