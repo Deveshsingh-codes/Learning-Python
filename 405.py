000a3032c2be91f5820ef75e8510444355a43802
Devesh Singh <zero2march@gmail.com>
2026-06-22 23:29:56 +0530

    Update 405.py

--- 405.py
+++ 405.py
@@ -1,8 +1,12 @@
-# OOPS
+ # CLASS METHODS IN PYTHON
+ 
+class Employee():
+    no_of_leaves=8
+    
+    def __init__(self, aname, asalary, arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
 
-
-class Student:
-    pass
-
-
-
+    def printdetails(self):
+        return f"The name is {self.name}, salary is {self.salary}, role is {self.role}"
