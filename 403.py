b3a4d373ea4ab3a3cd7d68bfe4a39935aee3d8e2
Devesh Singh <zero2march@gmail.com>
2026-06-22 22:26:25 +0530

    Updaet 403.py

--- 403.py
+++ 403.py
@@ -10,3 +10,15 @@ Rohan=Employee()
 
 Harry.name="Harry"
 Rohan.name="Rohan"
+Harry.salary=400
+Rohan.salary=500
+Harry.role="Instructor"
+Rohan.role="Student"
+
+print(Harry.salary)  # 400
+print(Rohan.salary)  #500
+print(Harry.role, Rohan.role) #   Instructor Student
+
+
+print(Harry.no_of_leaves)   # 8 ----> Class ki property ko Harry ka use krke access kr liya 
+
