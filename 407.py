368b81000aa7e7b19cb912a58f79184dabc0462c
Devesh Singh <zero2march@gmail.com>
2026-06-25 00:09:07 +0530

    Update 407.py

--- 407.py
+++ 407.py
@@ -18,10 +18,18 @@ class Employee():
     def printgood(string):
         print("This is a good " + string)
         
+        #return 98 ---------
+                                                                     
+# Ek esa function bnana hai jo na self ko as an argument le
+#na class ko ...khud apne aap me kaam kre...
+
+        
 Harry=Employee("Harry",400,"Instructor")
 Rohan=Employee("Rohan",500,"Student")
 Karan =Employee.from_str("Karan-600-Teacher")
 print(Karan.printgood("Harry")) 
 
 #This is a good Harry
-#None
+#None      -----> isne kuc bhi return nhi kiya is liye none aaya output me 
+
+# ----------Ab ye none ki jgh 98 dega--->
