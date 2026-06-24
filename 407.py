f4480b12454e7480f9ed5d622d25aa430f90c8b5
Devesh Singh <zero2march@gmail.com>
2026-06-24 23:53:50 +0530

    Update 407.py

--- 407.py
+++ 407.py
@@ -1,1 +1,15 @@
 # Static method
+class Employee():
+    no_of_leaves=8
+    
+    def __init__(self, aname, asalary, arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+    def printdetails(self):
+        return f"The name is {self.name}, salary is {self.salary}, role is {self.role}"
+    
+    @classmethod
+    def from_str(cls,string):
+        return cls(*string.split("-"))
