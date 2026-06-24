406d3e3b10fcc6eda8613772d6b0e56a6413df09
Devesh Singh <zero2march@gmail.com>
2026-06-24 23:16:27 +0530

    Update 405.py

--- 405.py
+++ 405.py
@@ -12,6 +12,15 @@ class Employee():
         return f"The name is {self.name}, salary is {self.salary}, role is {self.role}"
     
     @classmethod
-    def change_leaves(cls,newleave):
+    def change_leaves(cls,newleave):  # by this method we will access object  by any instance or class
         cls.no_of_leaves=newleave
+Harry=Employee("Harry",400,"Instructor")
+Rohan=Employee("Rohan",500,"Student")
+
+Harry.change_leaves(34)
+Employee.change_leaves(90)   #Ye bhi same class variable ko modify kar raha hai
+print(Harry.no_of_leaves)  # 90 
+
+#<----------------confusssion----------->
+
         
