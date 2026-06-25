75379d6a48935e9bcbe73611ca92c7170f1407a5
Devesh Singh <zero2march@gmail.com>
2026-06-25 23:22:58 +0530

    Update Excercise-08.py

--- Excercise-08.py
+++ Excercise-08.py
@@ -4,3 +4,14 @@ from win32com.client import Dispatch
 speak = Dispatch("SAPI.spVoice")
 
 speak.Speak("Harry bhai How are you")
+
+
+def speak(str):
+    from win32com.client import Dispatch
+    
+    speak=Dispatch("SAPI.spVoice")
+    
+    speak.Speak("str")
+    
+if __name__ == ' __main__ ':
+    speak("Harry bhai How are you")
