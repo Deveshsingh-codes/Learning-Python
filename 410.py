5ac2dafa368143a1b3b4c8bc4e962bbc515d34ad
Devesh Singh <zero2march@gmail.com>
2026-06-26 23:40:42 +0530

    Update 410.py

--- 410.py
+++ 410.py
@@ -1,1 +1,20 @@
 # Multiple inharitance
+
+class Employee():
+    no_of_leaves=8
+    
+    def __init__(self, aname, asalary, arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+    def printdetails(self):
+        return f"The name is {self.name}, salary is {self.salary}, role is {self.role}"
+    
+    @classmethod
+    def from_str(cls,string):
+        return cls(*string.split("-"))
+    
+    @staticmethod
+    def printgood(string):
+            print("This is a good " + string)
