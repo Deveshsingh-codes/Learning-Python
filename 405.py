62a8575d1e34843b917a76e6bc88f2bda5378bae
Devesh Singh <zero2march@gmail.com>
2026-07-06 15:14:49 +0530

    Update 405.py

--- 405.py
+++ 405.py
@@ -26,7 +26,7 @@ print(Harry.no_of_leaves)  # 90
 Harry.change_leaves(34)
 Employee.change_leaves(90)   #Ye bhi same class variable ko modify kar raha hai
 Rohan.change_leaves(50)
-print(Harry.no_of_leaves) 
+print(Harry.no_of_leaves)  # 50
     
 #internally Employee.change_leaves(60) ban jata hai To class variable fir se update:Employee.no_of_leaves = 60
 #Ab Harry aur Rohan dono ke liye value 60 hai....
