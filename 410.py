c8011b6c2064ad4dbf80dc55b3c57ef14df36907
Devesh Singh <zero2march@gmail.com>
2026-07-06 17:20:54 +0530

    Update 410.py

--- 410.py
+++ 410.py
@@ -20,20 +20,26 @@ class Employee():
             print("This is a good " + string)
 
 class Player:
+    var = 9
     no_of_games = 4
     def __init__(self,name,game):
-        self.name=name 
+        self.name=name  # these are the two attributes of the class Player 
         self.game = game         
         
     def printdetails(self):
         return f"The Name is {self.name} and the game is  {self.game}."
 class coolprogram(Employee, Player):
-    pass     
+    var = 10
+    language = "C++"
+    def printlanguage(self):
+        print(self.language)  
+          
 Harry = Employee("Harry",400,"Instructor")
 Rohan = Employee("Rohan",500,"Student")
 Subham = Player("Subham",["Cricket"])
-Karan = coolprogram("Karan",600,"Cool Programmer")
-class coolprogram(Employee, Player):
-    language = "C++"
-    def printlanguage(self):
-        print(self.language)
+Karan = coolprogram("Karan",900,"Cool Programmer")
+det = Karan.printdetails()  # The name is Karan, salary is 900, role is Cool Programmer
+print(det)
+print(Karan.var)  #10  #ye employee ki class ka var print karega
+                       #kyuki coolprogram me var=10 hai aur 
+                       #employee me var=9 hai to coolprogram ka var print hoga
