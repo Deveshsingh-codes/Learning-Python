bdf99899318e0ba822cb7bd83f1adcec92b8227c
Devesh Singh <zero2march@gmail.com>
2026-07-13 16:36:17 +0530

    Update 414.py

--- 414.py
+++ 414.py
@@ -1,6 +1,7 @@
 # Diamond Shape Problem in Multiple inheritance
+# Answer--> The DSP is an ambiguity that arises in object-oriented programming when a class inherits from two classes that have a common base class. This can lead to confusion about which method or attribute to use from the base class.
 class A:
-    def med(self):
+    def met(self):
         print("This is a method from class A")
 class B(A):
     pass
@@ -11,6 +12,22 @@ class D(B, C):
 a=A() # This is a parent class
 b=B() # this is a child class of A
 c=C() # this is a child class of A
-d=D() # this is a child class of B and 
-d.med() # This will call the method from class A due to method resolution order (MRO)
+d=D() # this is a child class of B and C
+d.met() # This will call the method from class A due to method resolution order (MRO)
 print(D.__mro__) # This will display the method resolution order
+
+#----------------------------------------------------------------->
+
+class A:
+    def met(self):
+        print("This is a method from class A")
+class B(A):
+    def met(self):
+        print("This is a method from class B")
+class C(A):
+    pass
+class D(B, C):
+    pass
+d.met() # This will call the method from class B due to method resolution order (MRO)
+print(D.__mro__) # This will display the method resolution order
+                
