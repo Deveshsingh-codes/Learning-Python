c00793232a57b130fe40e57ba82b2c9849a0fcc3
Devesh Singh <zero2march@gmail.com>
2026-07-13 16:50:16 +0530

    Update 415.py

--- 415.py
+++ 415.py
@@ -1,1 +1,6 @@
-# OPERATING OVERLOADING [ DINDER METHOD ]
+# OPERATING OVERLOADING [ DUNDER METHOD ]
+
+# Dunder method = in this method, Dunder methods (short for double underscore methods) are special, built-in methods in Python that begin and end with two underscores. Also known as magic methods, they allow you to hook into Python's core syntax and overload standard operations—such as addition, string conversion, or indexing—for your custom classes. Python invokes these methods implicitly behind the scenes when you use built-in functions or operators.
+# mapping perators to function
+class employee:
+    no_of_leaves=8
