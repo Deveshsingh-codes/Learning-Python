424365d154083232763576636b5d45b5dc03cbb1
Devesh Singh <zero2march@gmail.com>
2026-08-09 13:06:09 +0530

    Update exc 07

--- Excercise-07.py
+++ Excercise-07.py
@@ -1,4 +1,7 @@
 # Akhbar padhke sunao
+
+# for windows use --->
+
 from win32com.client import Dispatch
 
 speak = Dispatch("SAPI.spVoice")
@@ -14,4 +17,14 @@ def speak(str):
     speak.Speak("str")
     
 if __name__ == ' __main__ ':
+    speak("Harry bhai How are you")
+
+#for mac user-->
+
+import subprocess
+
+def speak(text):
+    subprocess.run(["say", text])
+
+if __name__ == "__main__":
     speak("Harry bhai How are you")
